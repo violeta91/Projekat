7331c9b6342b0af5ebc4472f13023d77ecec0f94
Violeta <>
2015-01-03 19:17:49 +0100

    Inicijalna verzija projekta. Postavljen je samo jframe.

---
+++ src/arkanoid/Frame.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arkanoid;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ * @author Violeta
+ */
+class Frame extends JFrame
+{   
+    public Frame()
+    {  
+        setJMenuBar(initMenu());
+        
+        pack();
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setTitle("Arkanoid");
+        
+        setVisible(true);
+    }
+    
+    /**
+     * @return menuBar
+     */
+    final JMenuBar initMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        
+        JMenu gameMenu = new JMenu("Game");
+        
+        JMenuItem newGame = new JMenuItem("New game");
+        
+        gameMenu.add(newGame);
+        
+        menuBar.add(gameMenu);
+        
+        return menuBar;
+    }
+}
