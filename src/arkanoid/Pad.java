a8ada265fbc37661e2ab733ee769531a3c63c9ba
Violeta <>
2015-01-17 22:33:39 +0100

    Dodati komentari u klasi pad.

--- src/arkanoid/Pad.java
+++ src/arkanoid/Pad.java
@@ -33,6 +33,13 @@ class Pad extends Rectangle.Double implements GameObject
     
     private Color fillColor = Color.BLACK;
     
+    /**
+     * Inicijalizuje reket na prosedjenoj lokaciji na tabli.
+     * 
+     * @param board Tabla kojoj reket pripada.
+     * @param x x koordinata gornjeg levog temena reketa.
+     * @param y y koordinata gornjeg leveg temena reketa.
+     */
     public Pad(Board board, int x, int y) {
         this.board = board;
         this.x = x;
@@ -42,37 +49,55 @@ class Pad extends Rectangle.Double implements GameObject
         this.state = MovingState.STANDING;
     }
     
+    /**
+     * Postavlja stanje kretanja reketa u desno.
+     */
     public void moveRight() {
         state = MovingState.MOVING_RIGHT;
     }
     
+    /**
+     * Postavlja stanje kretanja reketa u levo.
+     */
     public void moveLeft() {
         state = MovingState.MOVING_LEFT;
     }
     
+    /**
+     * Postavlja stanje kretanja reketa u stajanje.
+     */
     public void stopMoving() {
         state = MovingState.STANDING;
     }
     
-    @Override
+    /**
+     * Izvrsava pomeranje reketa u zavisnosti od stanja.
+     */
     public void move() {
         if (state == MovingState.MOVING_RIGHT)
             x += dx;
         else if (state == MovingState.MOVING_LEFT)
             x -= dx;
         
-        if (x < 0)
+        if (x < 0) //slucaj kada je skroz levo reket
             x = 0;
-        else if (x + w > board.PANEL_WIDTH)
+        else if (x + w > board.PANEL_WIDTH) //slucaj kada je skoz desno
             x = board.PANEL_WIDTH - w;
     }
     
+    /**
+     * Funkcija vrsi resetovanje koordinata za iscrtavanje reketa.
+     */
     public void reset()
     {
         this.x = board.PANEL_WIDTH/2 - Pad.w/2;
     }
     
-    @Override
+    /**
+     * Iscrtava reket na tabli.
+     * 
+     * @param g2 Graphics2D objekat na kome se vrsi iscrtavanje.
+     */
     public void draw(Graphics2D g2) {
         g2.setPaint(fillColor);
         g2.fill(this);
